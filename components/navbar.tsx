103f9e4272a7628ec1710324418a1e52c2f7248f
leedonghyun1 <ehdgus4119@naver.com>
2023-08-25 13:19:47 +0900

    navbar isPro fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,13 +5,17 @@ import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
 import { getApiLimitCount } from "@/lib/api-limit";
 
-const NavBar = async() => {
+interface MobileSideBar{
+  isPro:boolean;
+}
+
+const NavBar = async({isPro=false}) => {
 
   const apiLimitCount = await getApiLimitCount();
 
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar apiLimitCount={apiLimitCount} />
+      <MobileSidebar apiLimitCount={apiLimitCount} isPro={isPro}  />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
